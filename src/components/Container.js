f3c86c6c350f126116d69fb41c7a5977902008de
araj54657 <araj65841@gmail.com>
2023-11-29 11:12:08 +0530

    add v

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,7 +6,7 @@ const Container = () => {
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
   if (!movies) return;
   const mainMovie = movies[0];
-  console.log(mainMovie);
+
   const { original_title, overview, id } = mainMovie;
   return (
     <div>
--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import { addTrailerVideo } from "../utils/movieSlice";
 import { API_OPTIONS } from "../utils/constants";
 
-const useMovieTrailer = ({ movieId }) => {
+const useMovieTrailer = (movieId) => {
   const dispatch = useDispatch();
   //fetch trailer
   const getMovieVideos = async () => {
@@ -14,10 +14,10 @@ const useMovieTrailer = ({ movieId }) => {
       API_OPTIONS
     );
     const json = await data.json();
-    console.log(json);
+
     const filterData = json.results.filter((video) => video.type === "Trailer");
     const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
+
     dispatch(addTrailerVideo(trailer));
   };
   useEffect(() => {
